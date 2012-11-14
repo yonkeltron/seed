572ca7ca1e9de6dc5bdf6c37e27125b18005dcdf
yonkeltron <yonkeltron@gmail.com>
2012-11-14 15:55:37 -0500

    fixed a broken conditional and streamlined curl/puppet installation
    Signed-off-by: yonkeltron <yonkeltron@gmail.com>

--- seed.zsh
+++ seed.zsh
@@ -3,38 +3,45 @@
 echo "Seed script beginning at $date"
 
 # get curl installed
+
 function {
-    local curl_path=$(which_curl);
-    if [[ -x $curl_path ]]; then
-        echo "Good, you've got curl!"
-        echo $(curl --version)
+    packages_to_install=(curl puppet);
+    package_list="";
+
+    for package in $packages_to_install; do
+        local package_path=$(which $package);
+
+        echo
+
+        if [[ -x $package_path ]]; then
+            echo "Good, you've got $package installed:"
+            echo $($package --version)
+        else
+            echo "You need $package installed. We'll install it."
+            package_list+="$package ";
+        fi
+    done
+
+    if [[ -n $package_list ]]; then
+        echo "You need to install $package_list so we'll do that now."
+        sudo aptitude install $package_list
     else
-        echo "You need curl installed. Trying to install now..."
-        sudo aptitude install curl
+        echo "Nothing to install, looks like you've got all you need.";
     fi
 }
 
-# get puppet installed
+# let puppet install packages needed for rvm
 function {
-    local puppet_path=$(which puppet);
-    if [[ -x $puppet_path ]]; then
-        echo "Good, you've got puppet: $(puppet --version)"
-    else
-        echo "You need puppet installed. Trying to install now..."
-        sudo aptitude install puppet
-    fi
-
-    # let puppet install packages needed for rvm
     echo "Now installing base packages with puppet. This should include all you need to get going with the rest of the seed."
     sudo puppet apply build_packages.pp
 }
 
 # get rvm installed
 function {
-    local rvm_path=$(which rvm)
-    if [[ -x $rvm_path ]]; then
+    local rvm_path=$HOME/.rvm
+    if [[ (-d $rvm_path) && (-x $rvm_path/scripts/rvm) ]]; then
         echo "Good, you've got rvm!"
-        rvm --version
+        echo $(rvm --version)
     else
         echo "Installing RVM..."
         \curl -L https://get.rvm.io | zsh -s stable
@@ -47,6 +54,7 @@ source $HOME/.rvm/scripts/rvm
 # actually install a ruby
 echo "Installing ruby stable..."
 rvm install ruby
+echo
 echo "Looks like we've got $(ruby --version)"
 
 # install gems needed by irbrc and config
